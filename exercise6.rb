88f5ef503cbe54d03d51ab6baa43c15ef5e8627e
Colin Suckow <csuckow99@gmail.com>
2021-05-09 21:50:20 -0700

    Replace foreground color with method

--- exercise6.rb
+++ exercise6.rb
@@ -15,9 +15,17 @@ class Button
 
   def draw
     if @is_dark_mode
-      paint(@label, @x, @y, @foreground_color - 10, '#111111')
+      paint(@label, @x, @y, foreground_color, '#111111')
     else
-      paint(@label, @x, @y, @foreground_color + 10, '#E0E0E0')
+      paint(@label, @x, @y, foreground_color, '#E0E0E0')
+    end
+  end
+
+  def foreground_color
+    if @is_dark_mode
+      @foreground_color - 10
+    else
+      @foreground_color + 10
     end
   end
 
