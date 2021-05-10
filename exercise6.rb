63162c43f741d6a36a785b4f4d1d53edd8bf595e
Colin Suckow <csuckow99@gmail.com>
2021-05-09 21:46:35 -0700

    Extract method into new class

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,24 @@
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-  if is_dark_mode
-    # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
-  else
-    # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+  Button.new(label_text, x, y, foreground_color, is_dark).draw
+end
+
+class Button
+  attr_reader :label, :x, :y, :foreground_color, :is_dark_mode
+
+  def initialize(label, x, y, foreground_color, is_dark_mode)
+    @label = label
+    @x = x
+    @y = y
+    @foreground_color = foreground_color
+    @is_dark_mode = is_dark_mode
   end
+
+  def draw
+    if @is_dark_mode
+      paint(@label, @x, @y, @foreground_color - 10, '#111111')
+    else
+      paint(@label, @x, @y, @foreground_color + 10, '#E0E0E0')
+    end
+  end
+
 end
